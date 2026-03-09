528b67db1c99f658be4450261ad7c4fb58a61ad4
ChanoPark <koc081900@korea.ac.kr>
2026-03-09 23:00:52 +0900

    INIT: 프로젝트 생성

---
+++ src/main/kotlin/com/chanos/avatingcore/AvatingCoreApplication.kt
@@ -0,0 +1,11 @@
+package com.chanos.avatingcore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AvatingCoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<AvatingCoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/chanos/avatingcore/AvatingCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chanos.avatingcore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvatingCoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
